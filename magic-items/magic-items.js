7bd7b28a84ed772d77fae2621dcf8427c1a5c114
Erik Ford <ekford18@gmail.com>
2020-04-06 14:19:39 -0700

    created magic items js file

---
+++ magic-items/magic-items.js
@@ -0,0 +1,64 @@
+const bagOfHolding = {
+    id: 'bagOfHolding',
+    name: 'Bag of Holding',
+    image: 'bag-of=holding.jpg',
+    description: 'The bag of holding opens into a nondimensional space: its inside is larger than its outside dimensions.',
+    category: 'Wonderous Item',
+    price: 400
+};
+
+const slippersOfSpiderClimbing = {
+    id: 'slippersOfSpiderClimbing',
+    name: 'Slippers of Spider Climbing',
+    image: 'slippers-of-spider-climbing.jpg',
+    description: 'Slippers of spider climbing are magical shoes that allow their wearer to effortlessly walk on vertical surfaces, or upside-down along ceilings, leaving their hands free',
+    category: 'Wonderous Item',
+    price: 600
+};
+
+const hornOfValhalla = {
+    id: 'hornOfValhalla',
+    name: 'Horn of Valhalla',
+    image: 'horn-of-valhalla.jpg',
+    description: 'Horn of Valhalla has the power to summon forth the spirits of barbarians from their afterlife to fight for the summoner.',
+    category: 'Wonderous Item',
+    price: 1000
+};
+
+const ringOfSpellStoring = {
+    id: 'ringOfSpellStoring',
+    name: 'Ring of Spell Storing',
+    image: 'ring-of-spell-storing.jpg',
+    description: 'This ring stores spells cast into it, holding them until the user uses them.',
+    category: 'Ring',
+    price: 700
+};
+
+const immovableRod = {
+    id: 'immovableRod',
+    name: 'Immovable Rod',
+    image: 'immovable-rod.jpg',
+    description: 'This rod is a flat iron bar with a small button on one end. When the button is pushed, the rod does not move from where it is, even if staying in place defies gravity.',
+    category: 'Wonderous Item',
+    price: 1000
+};
+
+const glamourArmor = {
+    id: 'glamourArmor',
+    name: 'Galmour Armor',
+    image: 'glamour-armor.jpg',
+    description: 'This beautiful studded leathor armor has the ability to change into a set of clothes. All you have to do is provide the command word!',
+    category: 'Armor',
+    price: 800
+};
+
+const ringOfTheRam = {
+    id: 'ringOfTheRam',
+    name: 'Ring of the Ram',
+    image: 'ring-of-the-ram',
+    description: 'The ring of the ram is an ornate ring forged of hard metal. It has the head of a ram as its device. The wearer can use the ring to give forth a ram-like force, manifested by a vaguely discernible shape that resembles the head of a ram.',
+    category: 'Ring',
+    price: 600
+};
+
+export default magicItems;
