b4f360d38ab6bcd8babe05da21fac79bdc1b3581
Erik Ford <ekford18@gmail.com>
2020-04-07 14:03:13 -0700

    added magic items data card

---
+++ magic-items/magic-items-data-cart.js
@@ -0,0 +1,24 @@
+const cart = [{
+    id: 'bagOfHolding',
+    quantity: 2
+}, {
+    id: 'slippersOfSpiderClimbing',
+    quantity: 1
+}, {
+    id: 'hornOfValhalla',
+    quantity: 1
+}, {
+    id: 'ringOfSpellStoring',
+    quantity: 3
+}, {
+    id: 'immovableRod',
+    quantity: 1
+}, {
+    id: 'glamourArmor',
+    quantity: 4
+}, {
+    id: 'ringOfTheRam',
+    quantity: 3
+}];
+
+export default cart;
