ce384d9d35ccf6dbc8bbd37c1724d8dcbc2c8143
Erik Ford <ekford18@gmail.com>
2020-04-08 14:12:23 -0700

    local storage complete

--- functions/magic-item-table-creation.js
+++ functions/magic-item-table-creation.js
@@ -6,14 +6,32 @@ import calcOrderTotal from '../functions/order-total.js';
 
 const tbody = document.querySelector('tbody');
 const orderTotalSpot = document.getElementById('order-total');
+const purchaseButton = document.getElementById('purchase-button');
+const json = localStorage.getItem('CART');
+let newCart;
+if (json) {
+    newCart = JSON.parse(json);
+} else {
+    newCart = [];
+}
 
-for (let i = 0; i < cart.length; i++) {
-    const cartItem = cart[i];
+for (let i = 0; i < newCart.length; i++) {
+    const cartItem = newCart[i];
     const magicItem = findById(magicItems, cartItem.id);
     const updateTable = magicItemRow(cartItem, magicItem);
 
     tbody.appendChild(updateTable);
 }
 
-const orderTotal = calcOrderTotal(cart, magicItems);
+if (newCart.length === 0) {
+    purchaseButton.disabled = true;
+} else {
+    purchaseButton.addEventListener('click', () => {
+        localStorage.removeItem('CART');
+        alert('Youve ordered: ' + JSON.stringify(newCart, true, 2));
+        window.location = '../';
+    });
+}
+
+const orderTotal = calcOrderTotal(newCart, magicItems);
 orderTotalSpot.textContent = orderTotal;
